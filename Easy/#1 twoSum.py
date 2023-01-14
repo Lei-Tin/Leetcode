29f0e03e31f30212d79ebb8ec47eafdfd8ce2c6f
HLeiTR <sy.hung@mail.utoronto.ca>
2023-01-14 18:59:11 -0500

    Updated #1 in Easy

--- Easy/#1 twoSum.py
+++ Easy/#1 twoSum.py
@@ -1,13 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = j = 0
+        # Optimized solution
+        indices = {}
 
-        while i < len(nums):
-            j = i + 1
-            while j < len(nums):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        for i in range(len(nums)):
+            if nums[i] in indices:
+                # This means we have already seen the
+                # complemenet of this value inside the dictionary
+                return [indices[nums[i]], i]
 
-                j += 1
+            else:
+                indices[target - nums[i]] = i
 
-            i += 1
+        # Don't need extra return because it is guaranteed to have a solution
+
+        # i = j = 0
+        #
+        # while i < len(nums):
+        #     j = i + 1
+        #     while j < len(nums):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        #
+        #         j += 1
+        #
+        #     i += 1
