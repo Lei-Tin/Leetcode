09b0b7ba2072df7ac81729da8b1ef17463860bfc
HLeiTR <sy.hung@mail.utoronto.ca>
2023-01-17 22:10:14 -0500

    Updated #409 in Easy

--- Easy/#409 longestPalindrome.py
+++ Easy/#409 longestPalindrome.py
@@ -1,17 +1,32 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        occur = {}
+        # Optimized solution
+        oddSet = set()
 
         for char in s:
-            occur[char] = occur.get(char, 0) + 1
+            if char not in oddSet:
+                oddSet.add(char)
+            else:
+                oddSet.remove(char)
 
-        ans = 0
+        if len(oddSet) > 0:
+            return len(s) - len(oddSet) + 1
 
-        for char in occur:
-            if occur[char] >= 2:
-                ans += (occur[char] // 2) * 2
+        else:
+            return len(s)
 
-        if any(occur[char] % 2 == 1 for char in occur):
-            ans += 1
-
-        return ans
+        # occur = {}
+        #
+        # for char in s:
+        #     occur[char] = occur.get(char, 0) + 1
+        #
+        # ans = 0
+        #
+        # for char in occur:
+        #     if occur[char] >= 2:
+        #         ans += (occur[char] // 2) * 2
+        #
+        # if any(occur[char] % 2 == 1 for char in occur):
+        #     ans += 1
+        #
+        # return ans
