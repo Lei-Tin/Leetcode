e9cb645ab2bd95767009ddd96c1c18e6c2a2c39f
HLeiTR <sy.hung@mail.utoronto.ca>
2023-05-18 12:37:15 +0800

    Updated #1557 in Medium

--- Medium/#1557 findSmallestSetOfVertices.py
+++ Medium/#1557 findSmallestSetOfVertices.py
@@ -1,5 +1,6 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        # Solution 1
         # We can just find all the nodes that doesn't have any nodes pointing to it
         ends = set()
         for _, end in edges:
@@ -7,3 +8,8 @@ class Solution:
                 ends.add(end)
 
         return list(set(i for i in range(n)) - ends)
+
+        # Solution 2
+        # Return the nodes that have no nodes pointing to it
+        # can_reach = {v for _, v in edges}
+        # return [i for i in range(n) if i not in can_reach]
