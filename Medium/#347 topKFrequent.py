661e5355806fb5bbe2f838ccf269a86aff9b5165
HLeiTR <sy.hung@mail.utoronto.ca>
2023-05-22 20:40:38 +0800

    Optimized #347 in Medium

--- Medium/#347 topKFrequent.py
+++ Medium/#347 topKFrequent.py
@@ -1,21 +1,49 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        buckets = [[] for _ in range(len(nums))]
+        # Solution 1, O(n)
+        count = Counter(nums)
+        frequency = defaultdict(list)
 
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-
-        for num in count:
-            buckets[count[num] - 1].append(num)
-
-        j = len(nums) - 1
+        for c in count:
+            frequency[count[c]].append(c)
 
         ans = []
-        while len(ans) < k:
-            if len(buckets[j]) > 0:
-                ans.append(buckets[j].pop())
-            else:
-                j -= 1
+        for i in range(len(nums), -1, -1):
+            ans.extend(frequency[i])
 
-        return ans
+        return ans[:k]
+
+        # Solution 2, O(n log n)
+        # pq = []
+
+        # count = Counter(nums)
+
+        # for n in count:
+        #     heappush(pq, (-count[n], n))
+
+        # ans = []
+        # for i in range(k):
+        #     ans.append(heappop(pq)[1])
+
+        # return ans
+
+        # Solution 3, O(n)
+        # count = {}
+        # buckets = [[] for _ in range(len(nums))]
+        #
+        # for num in nums:
+        #     count[num] = count.get(num, 0) + 1
+        #
+        # for num in count:
+        #     buckets[count[num] - 1].append(num)
+        #
+        # j = len(nums) - 1
+        #
+        # ans = []
+        # while len(ans) < k:
+        #     if len(buckets[j]) > 0:
+        #         ans.append(buckets[j].pop())
+        #     else:
+        #         j -= 1
+        #
+        # return ans
