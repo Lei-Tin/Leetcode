dd43b0b43eb7f02a7577d4e53127ddf134544070
HLeiTR <sy.hung@mail.utoronto.ca>
2023-06-06 10:42:27 +0800

    Optimized #1502 in Easy

--- Easy/#1502 canMakeArithmeticProgression.py
+++ Easy/#1502 canMakeArithmeticProgression.py
@@ -1,14 +1,35 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        # O(n) optimization
         if len(arr) <= 1:
             return True
 
-        else:
-            arr.sort()
-            diff = arr[1] - arr[0]
+        smallest = min(arr)
+        largest = max(arr)
+        n = len(arr)
 
-            for i in range(len(arr) - 1):
-                if arr[i + 1] - arr[i] != diff:
-                    return False
-
+        if largest == smallest:
             return True
+
+        if (largest - smallest) % (n - 1) != 0:
+            return False
+
+        diff = (largest - smallest) / (n - 1)
+
+        if len(set(arr)) != n:
+            return False
+
+        return all((num - smallest) % diff == 0 for num in arr)
+
+        # if len(arr) <= 1:
+        #     return True
+
+        # else:
+        #     arr.sort()
+        #     diff = arr[1] - arr[0]
+
+        #     for i in range(len(arr) - 1):
+        #         if arr[i + 1] - arr[i] != diff:
+        #             return False
+
+        #     return True
