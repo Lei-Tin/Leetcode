5bd995dda91e0a8a2b68fe5724debfec65d78c89
HLeiTR <sy.hung@mail.utoronto.ca>
2023-07-05 16:59:17 +0800

    Updated #1493 in Medium

--- Medium/#1493 longestSubarray.py
+++ Medium/#1493 longestSubarray.py
@@ -1,19 +1,44 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        prev, curr = 0, 0
+        # Solution 1: Sliding window
+        i = 0
+        j = 0
         ans = 0
+        n = len(nums)
 
-        for num in nums:
-            if num == 1:
-                curr += 1
-            else:
-                ans = max(ans, prev + curr)
-                prev = curr
-                curr = 0
+        num_zero = 0
 
-        ans = max(ans, prev + curr)
+        # Let the window to have at most one 0
+        while j < n:
+            if nums[j] == 0:
+                num_zero += 1
 
-        if all(nums[i] == 1 for i in range(len(nums))):
-            return ans - 1
+            if num_zero == 2:
+                while num_zero == 2:
+                    if nums[i] == 0:
+                        num_zero -= 1
+                    i += 1
+
+            ans = max(ans, j - i)
+            j += 1
 
         return ans
+
+        # Solution 2: Count runs
+        # prev, curr = 0, 0
+        # ans = 0
+
+        # for num in nums:
+        #     if num == 1:
+        #         curr += 1
+        #     else:
+        #         ans = max(ans, prev + curr)
+        #         prev = curr
+        #         curr = 0
+
+        # ans = max(ans, prev + curr)
+
+        # if all(nums[i] == 1 for i in range(len(nums))):
+        #     return ans - 1
+
+        # return ans
