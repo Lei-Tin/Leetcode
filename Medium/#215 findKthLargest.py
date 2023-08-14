0bfe50ac6ede457ef4a55016645bb63dd1a94e65
HLeiTR <sy.hung@mail.utoronto.ca>
2023-08-15 05:34:23 +0800

    Optimized #215 in Medium

--- Medium/#215 findKthLargest.py
+++ Medium/#215 findKthLargest.py
@@ -1,15 +1,25 @@
 class Solution:
+    # O(K) memory and O(NlogK) time
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pivot = random.choice(nums)
+        pq = []
+        for n in nums:
+            heappush(pq, n)
+            if len(pq) > k:
+                heappop(pq)
 
-        smaller = [num for num in nums if num < pivot]
-        equal = [num for num in nums if num == pivot]
-        larger = [num for num in nums if num > pivot]
+        return pq[0]
 
-        # If we found out that the right hand side, larger has more values than k, then we can just look on the right hand side
-        if len(larger) >= k:
-            return self.findKthLargest(larger, k)
-        elif k > len(larger) + len(equal):
-            return self.findKthLargest(smaller, k - len(larger) - len(equal))
-        else:
-            return equal[0]
+        # Quick select solution, O(n) time and O(1) space
+        # pivot = random.choice(nums)
+
+        # smaller = [num for num in nums if num < pivot]
+        # equal = [num for num in nums if num == pivot]
+        # larger = [num for num in nums if num > pivot]
+
+        # # If we found out that the right hand side, larger has more values than k, then we can just look on the right hand side
+        # if len(larger) >= k:
+        #     return self.findKthLargest(larger, k)
+        # elif k > len(larger) + len(equal):
+        #     return self.findKthLargest(smaller, k - len(larger) - len(equal))
+        # else:
+        #     return equal[0]
