8b914f7efd9ffb47b82a075ff809517f80947a84
HLeiTR <sy.hung@mail.utoronto.ca>
2023-09-08 14:08:56 -0400

    Optimized #118 in Easy

--- Easy/#118 generate.py
+++ Easy/#118 generate.py
@@ -1,21 +1,32 @@
 class Solution:
+    # More optimized solution
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 1:
-            return [[1]]
+        ans = [[1] * (i + 1) for i in range(numRows)]
 
-        if numRows == 2:
-            return [[1], [1, 1]]
+        for i in range(2, numRows):
+            for j in range(1, i):
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j]
 
-        res = [[1], [1, 1]]
+        return ans
 
-        for i in range(3, numRows + 1):
-            res.append([1] * i)
-
-        # Start from row 3 and onwards
-        for row in range(2, len(res)):
-
-            # Start from the second element to the 2nd to the last element
-            for i in range(1, len(res[row]) - 1):
-                res[row][i] = res[row - 1][i - 1] + res[row - 1][i]
-
-        return res
+    # Original solution
+    # def generate(self, numRows: int) -> List[List[int]]:
+    #     if numRows == 1:
+    #         return [[1]]
+    #
+    #     if numRows == 2:
+    #         return [[1], [1, 1]]
+    #
+    #     res = [[1], [1, 1]]
+    #
+    #     for i in range(3, numRows + 1):
+    #         res.append([1] * i)
+    #
+    #     # Start from row 3 and onwards
+    #     for row in range(2, len(res)):
+    #
+    #         # Start from the second element to the 2nd to the last element
+    #         for i in range(1, len(res[row]) - 1):
+    #             res[row][i] = res[row - 1][i - 1] + res[row - 1][i]
+    #
+    #     return res
