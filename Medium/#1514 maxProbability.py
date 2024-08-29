f18905bdfe1bc6e67ac00c05ef5624d543e8c856
HLeiTR <sy.hung@mail.utoronto.ca>
2024-08-29 17:41:37 -0400

    Updated #1514 in Medium

--- Medium/#1514 maxProbability.py
+++ Medium/#1514 maxProbability.py
@@ -1,24 +1,55 @@
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int,
+                       end_node: int) -> float:
         g = {i: [] for i in range(n)}
-        for e, w in zip(edges, succProb):
-            g[e[0]].append((e[1], w))
-            g[e[1]].append((e[0], w))
+        for i, (u, v) in enumerate(edges):
+            g[u].append((v, succProb[i]))
+            g[v].append((u, succProb[i]))
 
-        q = deque([(start, 1)])
+        # TLE Backtrack solution
+        # visited = {start_node}
 
-        visited = {i: 0 for i in range(n)}
+        # # backtrack
+        # def explore(curr, curr_prob):
+        #     if curr == end_node:
+        #         return curr_prob
 
-        while len(q) > 0:
-            n, c = q.popleft()
+        #     max_ans = 0
 
-            if visited[n] > c:
-                continue
-            else:
-                visited[n] = c
+        #     neighbors = g[curr]
+        #     for neighbor, cost in neighbors:
+        #         if neighbor not in visited:
+        #             visited.add(neighbor)
+        #             max_ans = max(max_ans, explore(neighbor, curr_prob * cost))
+        #             visited.remove(neighbor)
 
-            for neighbor, w in g[n]:
-                if visited[neighbor] < c * w:
-                    q.append((neighbor, c * w))
+        #     return max_ans
 
-        return visited[end]
+        # return explore(start_node, 1)
+
+        # dijkstra solution
+        pq = []
+        heapq.heappush(pq, (-1, start_node))
+
+        reach = [0] * n
+
+        reach[start_node] = 1
+
+        while len(pq) > 0:
+            cost, curr_node = heapq.heappop(pq)
+
+            cost = -cost
+
+            if curr_node == end_node:
+                return cost
+
+            for neighbor, prob in g[curr_node]:
+                # Update value to reach if it is more likely (higher probability)
+                new_cost = prob * cost
+
+                if new_cost > reach[neighbor]:
+                    reach[neighbor] = new_cost
+                    heapq.heappush(pq, (-new_cost, neighbor))
+
+        return 0
+
